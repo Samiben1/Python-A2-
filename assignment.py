37a4c38ff2c2f1f34a12142c6bb8ebff289974cb
Samiben1 <samiben498@gmail.com>
2019-09-25 00:49:19 +1000

    files

--- assignment.py
+++ assignment.py
@@ -1,4 +1,10 @@
 import openpyxl
 import sqlite3
 
-wb = openpyxl.load_workbook("")
+wb1 = openpyxl.load_workbook("inspections.xlsx")
+wb2 = openpyxl.load_workbook("violations.xlsx")
+
+sheet1 = wb.active
+sheet2 = wb.active
+
+print(sheet1)
